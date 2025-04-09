671138e894feba3c7175aad2b9c727d43b4685b8
Bastian Christensen <167467994+BastianChristensen@users.noreply.github.com>
2025-04-09 13:57:51 +0200

    Changed .md to .astro for project pages

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -4,13 +4,13 @@ export const projects = [
     description: "I'm making a game using Godot Game Engine!",
     tags: ["game-dev", "python"],
     image: "/images/godotgame1.png",
-    url: "/projects/proj-2"
+    url: "/projects/pro2"
   },
   {
     title: "This Website",
     description: "I build this website myself using Astro!",
     tags: ["web", "development"],
     image: "/images/Untitled design.png",
-    url: "/projects/proj-1"
+    url: "/projects/pro1"
   }
 ];
--- src/pages/posts/post-1.md
+++
@@ -1,7 +0,0 @@
----
-layout: ../../layouts/MarkdownPostLayout.astro
-title: My First Blog Post
-pubDate: 05/04/2025
-description: This is my first blog post.
----
-This is my first blog post about learning Astro.
