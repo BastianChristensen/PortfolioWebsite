d94f4a59cbfe0620d2eca2ad0bc68c186e148032
Bastian Christensen <167467994+BastianChristensen@users.noreply.github.com>
2025-04-10 15:28:13 +0200

    Content update

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -2,14 +2,14 @@ export const projects = [
   {
     title: "Godot Game",
     description: "I'm making a game using Godot Game Engine!",
-    tags: ["game-dev", "python"],
+    tags: ["game", "python"],
     image: "/images/share-image.webp",
     url: "/projects/pro2"
   },
   {
     title: "This Website",
     description: "I build this website myself using Astro!",
-    tags: ["web", "development"],
+    tags: ["web", "ongoing"],
     image: "/images/Untitled design.png",
     url: "/projects/pro1"
   }
