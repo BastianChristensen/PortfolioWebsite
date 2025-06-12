0dce71e420237285b82687d1f4c420adf2bbcad4
Bastian Christensen <167467994+BastianChristensen@users.noreply.github.com>
2025-06-12 02:48:16 +0200

    content update

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,5 +1,12 @@
 export const projects = [
   {
+    title: "Escape the Castle",
+    description: "Coming soon",
+    tags: ["python", "game", "ongoing"],
+    image: "/projectpics/escapethecastle/main_menu_image.png",
+    url: "/projects/pro3"
+  },
+  {
     title: "Budget App",
     description: "I've been working on a budget app to help me manage my finances better.",
     tags: ["python", "ongoing", "software development"],
