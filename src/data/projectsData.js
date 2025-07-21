d431f9731af0314132c57f834d8a983000c3d85f
Bastian Christensen <167467994+BastianChristensen@users.noreply.github.com>
2025-07-21 09:48:22 +0200

    removed project 3

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,11 +1,5 @@
 export const projects = [
-  {
-    title: "Escape the Castle",
-    description: "Coming soon",
-    tags: ["python", "game", "ongoing"],
-    image: "/projectpics/escapethecastle/main_menu_image.png",
-    url: "/projects/pro3"
-  },
+
   {
     title: "Budget App",
     description: "I've been working on a budget app to help me manage my finances better.",
